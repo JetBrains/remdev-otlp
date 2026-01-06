0f080391e99c325230850ec6e84dab65374a637c
Denis Zaichenko <denis.zaichenko@jetbrains.com>
2026-01-06 11:50:37 +0100

    Fixed race of conditions. Close main span before sending.

--- shared/src/main/kotlin/com/jetbrains/otp/span/DefaultRootSpanService.kt
+++ shared/src/main/kotlin/com/jetbrains/otp/span/DefaultRootSpanService.kt
@@ -1,6 +1,9 @@
 package com.jetbrains.otp.span
 
+import com.intellij.ide.AppLifecycleListener
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.Application
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import io.opentelemetry.api.GlobalOpenTelemetry
@@ -10,25 +13,36 @@ import io.opentelemetry.api.trace.Span
 import io.opentelemetry.context.Scope
 
 
+@Suppress("UnstableApiUsage")
 @Service(Service.Level.APP)
-class DefaultRootSpanService : Disposable {
+class DefaultRootSpanService : AppLifecycleListener {
     private val tracer = GlobalOpenTelemetry.get().getTracer("com.jetbrains.otp.diagnostic")
 
     @Volatile
-    private var currentSpan: Span
+    private var currentSpan: Span? = null
 
     @Volatile
-    private var currentScope: Scope
+    private var currentScope: Scope? = null
+
+    @Volatile
+    private var defaultSpan: Span? = null
+
+    @Volatile
+    private var defaultScope: Scope? = null
 
     init {
-        currentSpan = createApplicationSpan()
-        currentScope = currentSpan.makeCurrent()
+        ApplicationManager.getApplication().messageBus.simpleConnect().subscribe(AppLifecycleListener.TOPIC, this)
     }
 
     @Synchronized
     fun startSessionSpan(sessionId: String): Span {
-        currentScope.close()
-        currentSpan.end()
+        defaultScope?.close()
+        defaultSpan?.end()
+        defaultSpan = null
+        defaultScope = null
+
+        currentScope?.close()
+        currentSpan?.end()
 
         currentSpan = tracer.spanBuilder("remote-dev-session")
             .setAllAttributes(
@@ -38,36 +52,47 @@ class DefaultRootSpanService : Disposable {
             )
             .startSpan()
 
-        currentScope = currentSpan.makeCurrent()
+        currentScope = currentSpan!!.makeCurrent()
 
-        return currentSpan
+        return currentSpan!!
     }
 
     @Synchronized
     fun endSessionSpan() {
-        currentScope.close()
-        currentSpan.end()
-        currentSpan = createApplicationSpan()
-        currentScope = currentSpan.makeCurrent()
+        currentScope?.close()
+        currentSpan?.end()
+        currentSpan = null
+        currentScope = null
     }
 
-    private fun createApplicationSpan(): Span {
-        return tracer.spanBuilder("application-session")
-            .setAllAttributes(
-                Attributes.of(
-                    AttributeKey.stringKey("service.name"), "otp-diagnostic"
-                )
-            )
-            .startSpan()
+    @Synchronized
+    private fun getOrCreateDefaultSpan(): Span {
+        if (defaultSpan == null) {
+            defaultSpan = tracer.spanBuilder("application-init")
+                .startSpan()
+            defaultScope = defaultSpan!!.makeCurrent()
+        }
+        return defaultSpan!!
     }
 
-    override fun dispose() {
-        currentScope.close()
-        currentSpan.end()
+    override fun appWillBeClosed(isRestart: Boolean) {
+        currentScope?.close()
+        currentSpan?.end()
+        defaultScope?.close()
+        defaultSpan?.end()
     }
 
     companion object {
         fun getInstance(): DefaultRootSpanService = service()
-        fun currentSpan(): Span = getInstance().currentSpan
+
+        fun currentSpan(): Span {
+            val instance = getInstance()
+            instance.currentSpan?.let { return it }
+            val contextSpan = Span.current()
+            if (contextSpan.spanContext.isValid) {
+                return contextSpan
+            }
+            return instance.getOrCreateDefaultSpan()
+        }
     }
 }
