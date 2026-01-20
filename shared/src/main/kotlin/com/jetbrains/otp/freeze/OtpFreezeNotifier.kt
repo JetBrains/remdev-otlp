17185ea1f453ecec94750b9d9c38ccd38b66d785
Denis Zaichenko <denis.zaichenko@jetbrains.com>
2026-01-20 15:46:12 +0100

    Add stack traces with abbreviated packages to UI freeze spans

--- shared/src/main/kotlin/com/jetbrains/otp/freeze/OtpFreezeNotifier.kt
+++ shared/src/main/kotlin/com/jetbrains/otp/freeze/OtpFreezeNotifier.kt
@@ -3,11 +3,14 @@ package com.jetbrains.otp.freeze
 import com.intellij.diagnostic.FreezeNotifier
 import com.intellij.diagnostic.LogMessage
 import com.intellij.diagnostic.ThreadDump
+import com.intellij.openapi.diagnostic.IdeaLogRecordFormatter
 import com.jetbrains.otp.span.DefaultRootSpanService
 import io.opentelemetry.api.GlobalOpenTelemetry
 import io.opentelemetry.api.common.AttributeKey
 import io.opentelemetry.api.common.Attributes
 import io.opentelemetry.context.Context
+import java.io.PrintWriter
+import java.io.StringWriter
 import java.nio.file.Path
 import java.time.Instant
 
@@ -23,17 +26,36 @@ class OtpFreezeNotifier : FreezeNotifier {
     ) {
         val endTime = Instant.now()
         val startTime = endTime.minusMillis(durationMs)
-
         val span = tracer.spanBuilder("ui-thread-freeze")
             .setParent(Context.current().with(DefaultRootSpanService.currentSpan()))
             .setStartTimestamp(startTime)
             .setAllAttributes(
                 Attributes.of(
-                    AttributeKey.stringKey("message"), event.throwable.message,
+                    AttributeKey.stringKey("stackTrace"), getAbbreviatedStackTrace(event.throwable),
                     AttributeKey.longKey("duration.ms"), durationMs
                 )
             ).startSpan()
 
         span.end(endTime)
+    }
+
+    fun getAbbreviatedStackTrace(throwable: Throwable): String {
+        val sw = StringWriter()
+        throwable.printStackTrace(PrintWriter(sw))
+        return sw.toString().lines().joinToString("\n") { line ->
+            if (line.trimStart().startsWith("at ")) {
+                val abbreviated = IdeaLogRecordFormatter.smartAbbreviate(
+                    line.substringAfter("at ").substringBefore("(")
+                )
+                if (abbreviated != null) {
+                    line.replace(
+                        line.substringAfter("at ").substringBefore("("),
+                        abbreviated
+                    )
+                } else line
+            } else {
+                line
+            }
+        }
     }
 }
