0ea5f7687d3187f04996748341cdc78c7464cb54
Denis Zaichenko <denis.zaichenko@jetbrains.com>
2026-02-27 19:39:12 +0100

    Bump plugin version to 1.0.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.jetbrains.otp"
-version = "1.0.7"
+version = "1.0.8"
 
 dependencies {
     intellijPlatform {
--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
 import org.jetbrains.intellij.platform.gradle.ProductMode
 
-version = "1.0.7"
+version = "1.0.8"
 plugins {
     id("intellij-platform-remdev")
 }
