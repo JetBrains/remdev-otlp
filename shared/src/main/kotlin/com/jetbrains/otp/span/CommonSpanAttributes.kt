469de9d07a994e16a941cc3d9966e6ac4e3b8316
adwait-chavan <adwait.chavan@salesforce.com>
2026-03-05 11:31:33 -0800

    Fix rd.platform.type attribute to follow OTel conventions

--- shared/src/main/kotlin/com/jetbrains/otp/span/CommonSpanAttributes.kt
+++ shared/src/main/kotlin/com/jetbrains/otp/span/CommonSpanAttributes.kt
@@ -1,7 +1,7 @@
 package com.jetbrains.otp.span
 
 object CommonSpanAttributes {
-    const val RD_SIDE = "rd-side"
+    const val RD_SIDE = "rd.platform.type"
     const val SIDE_FRONTEND = "frontend"
     const val SIDE_BACKEND = "backend"
     const val HOST_NAME = "host.name"
