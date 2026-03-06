0580ada5463e21480b1ca293ea48ce1c399c70ec
adwait-chavan <adwait.chavan@salesforce.com>
2026-03-06 11:23:25 -0800

    Add debug logging to denylist filtering

--- shared/src/main/kotlin/com/jetbrains/otp/exporter/DenylistFilteringMetricExporter.kt
+++ shared/src/main/kotlin/com/jetbrains/otp/exporter/DenylistFilteringMetricExporter.kt
@@ -1,5 +1,6 @@
 package com.jetbrains.otp.exporter
 
+import com.intellij.openapi.diagnostic.Logger
 import io.opentelemetry.sdk.common.CompletableResultCode
 import io.opentelemetry.sdk.metrics.InstrumentType
 import io.opentelemetry.sdk.metrics.data.AggregationTemporality
@@ -17,7 +18,22 @@ class DenylistFilteringMetricExporter(
         val filteredMetrics = metrics.filter { metric ->
             MetricNameFilter.shouldExport(metric.name)
         }
+
+        val deniedCount = metrics.size - filteredMetrics.size
+        if (deniedCount > 0) {
+            LOG.debug("Filtered out $deniedCount denied metrics, exporting ${filteredMetrics.size} metrics")
+        }
+
+        if (filteredMetrics.isEmpty() && metrics.isNotEmpty()) {
+            LOG.debug("All ${metrics.size} metrics were denied, skipping export")
+            return CompletableResultCode.ofSuccess()
+        }
+
         return delegate.export(filteredMetrics)
+    }
+
+    companion object {
+        private val LOG = Logger.getInstance(DenylistFilteringMetricExporter::class.java)
     }
 
     override fun flush(): CompletableResultCode {
