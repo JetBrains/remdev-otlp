a7b4d67478f8b630963305815768c3f5c0b50331
Denis Zaichenko <denis.zaichenko@jetbrains.com>
2026-04-20 16:49:26 +0200

    Bump plugin version to 1.0.14.1

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 
-version = "1.0.13.1"
+version = "1.0.14.1"
 plugins {
     id("intellij-platform-remdev")
 }
