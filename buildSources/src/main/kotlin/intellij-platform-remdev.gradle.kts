4c30bf5b084947c2c8b5695f44802341ee16381d
adwait-chavan <adwait.chavan@salesforce.com>
2026-04-21 18:43:10 -0700

    Handle conflicting HTTP sender dependencies

--- buildSources/src/main/kotlin/intellij-platform-remdev.gradle.kts
+++ buildSources/src/main/kotlin/intellij-platform-remdev.gradle.kts
@@ -17,13 +17,32 @@ dependencies {
     compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:${SharedDependencies.KOTLIN_SERIALIZATION_LIBRARY}")
 
     compileOnly("io.opentelemetry:opentelemetry-sdk:${SharedDependencies.OPEN_TELEMETRY_LIBRARY}")
+
+    // Use platform's exporter infrastructure to avoid ServiceLoader conflicts
+    compileOnly("io.opentelemetry:opentelemetry-exporter-common:${SharedDependencies.OPEN_TELEMETRY_LIBRARY}")
+    compileOnly("io.opentelemetry:opentelemetry-exporter-otlp-common:${SharedDependencies.OPEN_TELEMETRY_LIBRARY}")
+    compileOnly("io.opentelemetry:opentelemetry-exporter-sender-jdk:${SharedDependencies.OPEN_TELEMETRY_LIBRARY}")
+
     implementation("io.opentelemetry:opentelemetry-exporter-otlp:${SharedDependencies.OPEN_TELEMETRY_LIBRARY}") {
+        // Exclude SDK modules - use platform's versions
         exclude(group = "io.opentelemetry", module = "opentelemetry-sdk")
         exclude(group = "io.opentelemetry", module = "opentelemetry-sdk-trace")
         exclude(group = "io.opentelemetry", module = "opentelemetry-sdk-common")
         exclude(group = "io.opentelemetry", module = "opentelemetry-sdk-metrics")
+        exclude(group = "io.opentelemetry", module = "opentelemetry-sdk-logs")
         exclude(group = "io.opentelemetry", module = "opentelemetry-api")
         exclude(group = "io.opentelemetry", module = "opentelemetry-context")
+
+        // Exclude exporter infrastructure - use platform's versions to avoid ServiceLoader conflicts
+        exclude(group = "io.opentelemetry", module = "opentelemetry-exporter-common")
+        exclude(group = "io.opentelemetry", module = "opentelemetry-exporter-otlp-common")
+
+        // Exclude HTTP sender implementations - use platform's JDK-based sender
+        exclude(group = "io.opentelemetry", module = "opentelemetry-exporter-sender-okhttp")
+        exclude(group = "io.opentelemetry", module = "opentelemetry-exporter-sender-jdk")
+        exclude(group = "com.squareup.okhttp3")
+        exclude(group = "com.squareup.okio")
+
         exclude(group = "org.jetbrains.kotlin")
         exclude(group = "org.jetbrains.kotlinx")
     }
