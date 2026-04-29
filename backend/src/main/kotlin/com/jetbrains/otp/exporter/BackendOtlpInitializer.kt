eabb0746f5a588f3a2c11a89d4f95008e175e1c7
Denis Zaichenko <denis.zaichenko@jetbrains.com>
2026-04-29 16:29:15 +0200

    Add `plugin.version` to OTLP span and metric attributes

--- backend/src/main/kotlin/com/jetbrains/otp/exporter/BackendOtlpInitializer.kt
+++ backend/src/main/kotlin/com/jetbrains/otp/exporter/BackendOtlpInitializer.kt
@@ -2,13 +2,19 @@ package com.jetbrains.otp.exporter
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
+import com.jetbrains.otp.DiagnosticPlugin
 import com.jetbrains.otp.span.CommonSpanAttributes
 import com.jetbrains.otp.span.CommonSpanAttributesState
 
 class BackendOtlpInitializer : ProjectActivity {
     override suspend fun execute(project: Project) {
         val config = OtlpConfigFactory.fromEnv()
-        CommonSpanAttributesState.put(CommonSpanAttributes.RD_SIDE, CommonSpanAttributes.SIDE_BACKEND)
+        val commonAttributes = mutableMapOf(
+            CommonSpanAttributes.RD_SIDE to CommonSpanAttributes.SIDE_BACKEND
+        )
+        DiagnosticPlugin.version()?.let { commonAttributes[CommonSpanAttributes.PLUGIN_VERSION] = it }
+        CommonSpanAttributesState.upsert(commonAttributes)
+
         CpuUsageMetricReporter.getInstance().start(CommonSpanAttributes.SIDE_BACKEND)
         FrequentPerformanceMetricsReporter.getInstance().start(CommonSpanAttributes.SIDE_BACKEND)
         TelemetrySpanExporter.getInstance().initExporter(config)
