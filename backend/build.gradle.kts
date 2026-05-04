c1409357a942492af9432c0aa786bbb948a13f91
Denis Zaichenko <denis.zaichenko@jetbrains.com>
2026-05-04 19:45:52 +0300

    Bump plugin version to 1.0.17

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
 import org.jetbrains.intellij.platform.gradle.ProductMode
 
-version = "1.0.16"
+version = "1.0.17"
 
 plugins {
     id("intellij-platform-remdev")
