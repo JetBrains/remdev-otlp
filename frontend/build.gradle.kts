8c8ba67d7f30526542e0e0263c3df622226dde73
Denis Zaichenko <denis.zaichenko@jetbrains.com>
2026-05-04 19:31:59 +0300

    Bump version to 1.0.17.3

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
 import org.jetbrains.intellij.platform.gradle.ProductMode
 
-version = "1.0.16.3"
+version = "1.0.17.3"
 plugins {
     id("intellij-platform-remdev")
 }
