e4c5a4f06cc2716b669cf195d1d86469a9161564
Denis Zaichenko <denis.zaichenko@jetbrains.com>
2026-05-28 15:54:01 +0200

    Bump plugin version to 1.0.19

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
 import org.jetbrains.intellij.platform.gradle.ProductMode
 
-version = "1.0.18"
+version = "1.0.19"
 
 plugins {
     id("intellij-platform-remdev")
--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "com.jetbrains.otp"
-version = "1.0.18"
+version = "1.0.19"
 
 val moduleDescriptorFiles = mapOf(
     "OtpDiagnostic.shared" to layout.projectDirectory.file("shared/src/main/resources/OtpDiagnostic.shared.xml"),
