32ddcba75815309fdc808884988eae28434989b0
Denis Zaichenko <denis.zaichenko@jetbrains.com>
2026-06-11 16:53:48 +0200

    Bump version to 1.0.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "com.jetbrains.otp"
-version = "1.0.20"
+version = "1.0.21"
 
 val moduleDescriptorFiles = mapOf(
     "OtpDiagnostic.shared" to layout.projectDirectory.file("shared/src/main/resources/OtpDiagnostic.shared.xml"),
