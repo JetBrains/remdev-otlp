6efb1af3ac9b25682621b7e501407fe77915daf9
Denis Zaichenko <denis.zaichenko@jetbrains.com>
2026-06-11 16:53:48 +0200

    Bump version to 1.0.22

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 
-version = "1.0.20"
+version = "1.0.22"
 plugins {
     id("intellij-platform-remdev")
 }
